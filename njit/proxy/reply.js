cdb6e9633d250410624e08cc87f94d1d4766cad1
Zhangyi Jin <billjzy637@gmail.com>
2015-11-02 07:06:36 -0500

    reply & blog proxy

---
+++ njit/proxy/reply.js
@@ -0,0 +1,18 @@
+var models = require('../models');
+var Reply = models.Reply;
+var User = require('./users');
+var tools = require('../common/tools');
+var EventProxy = require('eventproxy');
+var at = require('../common/at');
+
+
+//get one reply 
+exports.getReply = function(id, callback){
+
+};
+//get reply by its id
+exports.getReplyById = function(id, callback){
+
+};
+
+exports.getRepliesByBlogId
